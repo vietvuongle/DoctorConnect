d0c1642a2d7d505ce0d706c63febdb2c3c29c974
vietvuongle <vietvuongf@gmail.com>
2025-05-08 08:09:47 +0700

    update list doctor

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,42 +1,41 @@
-import React, { useContext } from 'react'
-import Navbar from './components/Navbar'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Sidebar from './components/Sidebar';
-import { Route, Routes } from 'react-router-dom';
-import { AdminContext } from './context/AdminContext';
-import { DoctorContext } from './context/DoctorContext';
-import Login from './pages/Login';
-import Home from './pages/admin/Home';
-import AddDoctor from './pages/admin/AddDoctor';
-import { Departments } from './pages/admin/Departments';
-
-
+import React, { useContext } from "react";
+import Navbar from "./components/Navbar";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import { AdminContext } from "./context/AdminContext";
+import { DoctorContext } from "./context/DoctorContext";
+import Login from "./pages/Login";
+import Home from "./pages/admin/Home";
+import AddDoctor from "./pages/admin/AddDoctor";
+import { Departments } from "./pages/admin/Departments";
+import ListDoctor from "./pages/admin/ListDoctor";
 
 function App() {
-  const {aToken} = useContext(AdminContext)
-  const {dToken} = useContext(DoctorContext)
+    const { aToken } = useContext(AdminContext);
+    const { dToken } = useContext(DoctorContext);
 
-
-  return aToken || dToken ? (
-    <div className='bg-[#F8F9FD]'>
-      <ToastContainer />
-      <Navbar />
-      <div className='flex items-start'>
-        <Sidebar />
-        <Routes>
-          <Route path='/admin/trangchu' element={<Home/>} />
-          <Route path='/admin/thembacsi' element={<AddDoctor/>} />
-          <Route path='/admin/khoa' element={<Departments/>} />
-        </Routes>
-      </div>
-    </div>
-  ) : (
-    <>
-      <Login />
-      <ToastContainer />
-    </>
-  )
+    return aToken || dToken ? (
+        <div className="bg-[#F8F9FD]">
+            <ToastContainer />
+            <Navbar />
+            <div className="flex items-start">
+                <Sidebar />
+                <Routes>
+                    <Route path="/admin/home" element={<Home />} />
+                    <Route path="/admin/add-doctor" element={<AddDoctor />} />
+                    <Route path="/admin/department" element={<Departments />} />
+                    <Route path="/admin/list-doctor" element={<ListDoctor />} />
+                </Routes>
+            </div>
+        </div>
+    ) : (
+        <>
+            <Login />
+            <ToastContainer />
+        </>
+    );
 }
 
-export default App
+export default App;
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,47 +1,45 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import StickyNote2OutlinedIcon from '@mui/icons-material/StickyNote2Outlined';
-
+import React from "react";
+import { assets } from "../assets/assets";
+import { NavLink } from "react-router-dom";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import StickyNote2OutlinedIcon from "@mui/icons-material/StickyNote2Outlined";
 
 const Sidebar = () => {
-  return (
-    <div className='min-h-screen bg-white border-r'>
-      <ul className='text-[#515151]'>
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin/trangchu'}>
-          <img src={assets.home_icon} alt="" />
-          <p className='hidden md:block'>Trang chủ</p>
-        </NavLink>
+    return (
+        <div className="min-h-screen bg-white border-r">
+            <ul className="text-[#515151]">
+                <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`} to={"/admin/home"}>
+                    <img src={assets.home_icon} alt="" />
+                    <p className="hidden md:block">Trang chủ</p>
+                </NavLink>
 
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin/lichkham'}>
-          <img src={assets.appointment_icon} alt="" />
-          <p className='hidden md:block'>Lịch khám</p>
-        </NavLink>
+                <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`} to={"/admin/appointment"}>
+                    <img src={assets.appointment_icon} alt="" />
+                    <p className="hidden md:block">Lịch khám</p>
+                </NavLink>
 
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin/danhsachbs'}>
-          <img src={assets.people_icon} alt="" />
-          <p className='hidden md:block'>Danh sách bác sĩ</p>
-        </NavLink>
+                <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`} to={"/admin/list-doctor"}>
+                    <img src={assets.people_icon} alt="" />
+                    <p className="hidden md:block">Danh sách bác sĩ</p>
+                </NavLink>
 
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin/thembacsi'}>
-          <img src={assets.add_icon} alt="" />
-          <p className='hidden md:block'>Thêm bác sĩ</p>
-        </NavLink>
+                <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`} to={"/admin/add-doctor"}>
+                    <img src={assets.add_icon} alt="" />
+                    <p className="hidden md:block">Thêm bác sĩ</p>
+                </NavLink>
 
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin/khoa'}>
-          <StickyNote2OutlinedIcon sx={{fontSize: 30, color: '#1c274c'}}/>
-          <p className='hidden md:block'>Danh sách khoa</p>
-        </NavLink>
+                <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`} to={"/admin/department"}>
+                    <StickyNote2OutlinedIcon sx={{ fontSize: 30, color: "#1c274c" }} />
+                    <p className="hidden md:block">Danh sách khoa</p>
+                </NavLink>
 
-        <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''}`} to={'/admin/chat'}>
-          <ChatBubbleOutlineIcon sx={{fontSize: 30, color: '#1c274c'}}/>
-          <p className='hidden md:block'>Chat</p>
-        </NavLink>
-      </ul>
+                <NavLink className={({ isActive }) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""}`} to={"/admin/chat"}>
+                    <ChatBubbleOutlineIcon sx={{ fontSize: 30, color: "#1c274c" }} />
+                    <p className="hidden md:block">Chat</p>
+                </NavLink>
+            </ul>
+        </div>
+    );
+};
 
-    </div>
-  )
-}
-
-export default Sidebar
+export default Sidebar;
