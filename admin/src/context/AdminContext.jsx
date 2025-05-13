6ee7add7d0e4021d520e6020897f1e491d02ab3a
Linh Nguyễn Xuân <xuanlinhdev.2208@gmail.com>
2025-05-13 09:14:09 +0700

    chỉnh sửa AdminContext

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -8,6 +8,21 @@ const AdminContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     const [aToken, setAToken] = useState(localStorage.getItem("aToken") ? localStorage.getItem("aToken") : "");
     const [departmentData, setDepartmentData] = useState([]);
+    const [doctorData, setDoctorData] = useState([]);
+
+    const getDoctorData = async () => {
+        try {
+            const { data } = await axios.get(backendUrl + "/api/admin/all-doctor");
+
+            if (data !== false) {
+                setDoctorData(data.result);
+            } else {
+                toast.error("Error");
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    };
 
     const getDepartment = async () => {
         try {
@@ -28,10 +43,14 @@ const AdminContextProvider = (props) => {
         aToken,
         setAToken,
         departmentData,
+        setDepartmentData,
+        doctorData,
+        getDepartment,
     };
 
     useEffect(() => {
         getDepartment();
+        getDoctorData();
     }, []);
 
     return <AdminContext.Provider value={value}>{props.children}</AdminContext.Provider>;
